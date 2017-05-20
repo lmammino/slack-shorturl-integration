743bea1b89b5620e1bb886ddf47d431106081d68
Luciano Mammino <lucianomammino@gmail.com>
2017-05-20 19:38:58 +0100

    Improved server

--- src/server.js
+++ src/server.js
@@ -6,19 +6,25 @@ const slashCommandFactory = require('./slashCommand')
 const app = new Express()
 app.use(bodyParser.urlencoded({extended: true}))
 
-const {SLACK_TOKEN: slackToken, REBRANDLY_APIKEY: apiKey} = process.env
+const {SLACK_TOKEN: slackToken, REBRANDLY_APIKEY: apiKey, PORT} = process.env
 
 if (!slackToken || !apiKey) {
   console.error('missing environment variables SLACK_TOKEN and/or REBRANDLY_APIKEY')
   process.exit(1)
 }
 
+const port = PORT || 80
+
 const rebrandlyClient = createShortUrlsFactory(apiKey)
 const slashCommand = slashCommandFactory(rebrandlyClient, slackToken)
 
 app.post('/', (req, res) => {
   slashCommand(req.body)
-    .then(res.json)
+    .then((result) => {
+      return res.json(result)
+    })
 })
 
-app.listen(3000, console.log)
+app.listen(port, () => {
+  console.log(`Server started at localhost:${port}`)
+})
